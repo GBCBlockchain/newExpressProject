c1996e9240b124c37fe990210317d73ac7da26cd
priom <priom.galaxy@gmail.com>
2018-10-11 21:54:53 -0400

    custom expressjs project created

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('new-expressjs-project:server');
 var http = require('http');
 
